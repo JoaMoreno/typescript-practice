a20df7d459618c4d4a622afb8ff563a2d1b9ed0d
Joaquin Moreno <tec.ignaciomoreno@gmail.com>
2020-02-29 23:20:49 -0300

    Implement Passport js

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,25 +1,32 @@
-import express from 'express'
-import morgan from 'morgan'
-import cors from 'cors'
+import express from "express";
+import morgan from "morgan";
+import cors from "cors";
+import passport from "passport";
 
-import authRoutes from './routes/auth.routes'
+import passportMiddleware from "./middlewares/passport";
+// Routes import
+import authRoutes from "./routes/auth.routes";
+import privateRoutes from "./routes/private.routes";
 
 // initializations
 const app = express();
 
 // settings
-app.set('port', process.env.PORT || 3000);
+app.set("port", process.env.PORT || 3000);
 
 // middlewares
-app.use(morgan('dev'));
-app.use(cors())
+app.use(morgan("dev"));
+app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
+app.use(passport.initialize());
+passport.use(passportMiddleware);
 
 // routes
-app.get('/', (req, res) =>{
-    res.send("Hola")
+app.get("/", (req, res) => {
+    res.send("Hola");
 });
 app.use(authRoutes);
+app.use(privateRoutes);
 
 export default app;
